1c8fa097d9fa3e179f04cc1b596c8c3a59cbab18
GUANTAI001 <jpguantai@gmail.com>
2023-02-28 06:06:07 +0300

    Created componets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Navigate , Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AirBnbPage from './components/AirBnbPage';
+import HotelPage from './components/HotelPage';
+import AccountPage from './components/AccountPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <BrowserRouter>
+
+      <Routes>
+      <Route path="/" element={<Navigate to="/home"/>} />
+     <Route path="/home" element={<HomePage/>}/>
+    <Route path="/airbnb" element={<AirBnbPage/>}/>
+    <Route path="/hotel" element={<HotelPage/>}/>
+    <Route path="/account" element={<AccountPage/>}/>
+      </Routes>
+
+    </BrowserRouter>
+   </>
   );
 }
 
---
+++ frontend/src/components/AirBnbPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AirBnbPage() {
+    return (
+        <div>
+        <h1>AirBnb Page</h1>
+        </div>
+    );
+    }
+
+export default AirBnbPage;
